3269ace55fb9fc5e13b9871184ea777d0a182722
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-02-15 11:09:22 +1300

    generator example

--- app.js
+++ app.js
@@ -17,7 +17,21 @@ const namesArr = ['Jack', 'John', 'Jill'];
 // Init iterator and pass in the names array
 const iterator = nameIterator(namesArr);
 
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+
+// Generator example
+function* sayNamees() {
+  yield 'Jack';
+  yield 'John';
+  yield 'Jill';
+}
+
+const names = sayNamees();
+
+console.log(names.next());
+console.log(names.next());
+console.log(names.next());
+console.log(names.next());
