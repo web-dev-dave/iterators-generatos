86161738cf3a0ab195ecf9bf22735a3f2beb9cf9
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-02-15 11:03:00 +1300

    sensible naming

---
+++ app.js
@@ -0,0 +1,23 @@
+// Iterator example
+function nameIterator(names) {
+  let nextIndex = 0;
+
+  return {
+    next: function () {
+      console.log(names.length);
+      return nextIndex < names.length
+        ? { value: names[nextIndex++], done: false }
+        : { done: true };
+    }
+  };
+}
+
+// Create an array of names
+const namesArr = ['Jack', 'John', 'Jill'];
+// Init iterator and pass in the names array
+const iterator = nameIterator(namesArr);
+
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
